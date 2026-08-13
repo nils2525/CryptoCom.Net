ae15914575e05195ad074012f10842cadd60ab3d
Jkorf <jankorf91@gmail.com>
2026-08-13 12:41:06 +0200

    Added Shared APIs Rest integration tests

--- CryptoCom.Net/Clients/ExchangeApi/CryptoComRestClientExchangeApiShared.cs
+++ CryptoCom.Net/Clients/ExchangeApi/CryptoComRestClientExchangeApiShared.cs
@@ -1191,7 +1191,7 @@ namespace CryptoCom.Net.Clients.ExchangeApi
 
         #region Open Interest client
 
-        GetOpenInterestOptions IOpenInterestRestClient.GetOpenInterestOptions { get; } = new GetOpenInterestOptions(_exchangeName, true);
+        GetOpenInterestOptions IOpenInterestRestClient.GetOpenInterestOptions { get; } = new GetOpenInterestOptions(_exchangeName, false);
         async Task<HttpResult<SharedOpenInterest>> IOpenInterestRestClient.GetOpenInterestAsync(GetOpenInterestRequest request, CancellationToken ct)
         {
             var validationError = SharedClient.GetOpenInterestOptions.ValidateRequest(request, this);
